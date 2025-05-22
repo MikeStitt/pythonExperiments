ea489e96d9256a4d6fc696abc6ef287aa8e94883
Mike Stitt <mike@stitt.cc>
2025-05-22 17:45:13 -0400

    Timing test for timedrobot.py
    Signed-off-by: Mike Stitt <mike@stitt.cc>

--- minTestRobot/robot.py
+++ minTestRobot/robot.py
@@ -1,66 +1,151 @@
+import gc
 import sys
-import wpilib
 
 from wpilib import RobotController
 from wpilib.timedrobotpy import TimedRobotPy
+from wpilib import TimedRobot
+
+import os
+
+def isEnvVarTrue(var_name):
+    """
+    Checks if an environment variable exists and evaluates to True.
+
+    Args:
+        var_name: The name of the environment variable.
+
+    Returns:
+        True if the variable exists and is considered true, False otherwise.
+    """
+    var_value = os.environ.get(var_name)
+
+    if var_value is None:
+        return False  # Variable does not exist
+
+    true_values = ['true', '1', 'yes', 'y']
+    return var_value.lower() in true_values
+
+_getFPGATime = RobotController.getFPGATime
+
+microsecondsAsInt = int
+
+PRINT_ENTRY_EXIT = isEnvVarTrue('MINTESTROBOT_PRINT_ENTRY_EXIT')
+if PRINT_ENTRY_EXIT:
+    def printEntry(name, startTimeUs:microsecondsAsInt):
+        print(f"{startTimeUs/1000_000.0:.6f}:Enter:{name}")
+
+    def printExit(name):
+        print(f"{_getFPGATime()/1000_000.0:.6f}:Exit :{name}")
+else:
+    def printEntry(name, startTimeUs):
+        pass
+    def printExit(name):
+        pass
+
 
 def printEntryAndExit(func):
     def wrapper(*args, **kwargs):
-        #name = inspect.currentframe().f_code.co_name
-        name = func.__name__
-        print(f"{RobotController.getFPGATime()/1000_000.0:.3f}:Enter:{name}")
+        name = args[1]
+        startTimeUs = _getFPGATime()
+        args[0].addCallLog(name,startTimeUs)
+        printEntry(name, startTimeUs)
         result = func(*args, **kwargs)
-        print(f"{RobotController.getFPGATime()/1000_000.0:.3f}:Exit :{name}")
+        printExit(name)
         return result
     return wrapper
 
-class MyRobot(TimedRobotPy):
+RobotParentClass = TimedRobotPy
+if isEnvVarTrue('MINTESTROBOT_USE_TIMEDRROBOT'):
+    RobotParentClass = TimedRobot
+
+
+
+
+NUM_TEST_PERIODIC = 10
+NUM_PERIODS = 100
+
+class MyRobot(RobotParentClass):
+
+    def addCallLog(self, name, timeUs):
+        if self._callCount[name] < NUM_PERIODS:
+            self._callTimesUs[name] += timeUs
+            self._callCount[name] +=1
 
     @printEntryAndExit
+    def testPeriodic(self,name):
+        if PRINT_ENTRY_EXIT:
+            print(f"testPeriodic({name})")
+        pass
+
     def startCompetition(self):
         super().startCompetition()
 
-    @printEntryAndExit
     def endCompetition(self):
         super().endCompetition()
+        avgDeltaCallTimesUs = {}
+        commonCallCount = self._callCount['loopStartTime']
+        for name, value in self._callTimesUs.items():
+            print(f"commonCallCount={commonCallCount}=self._callCount[{name}]={self._callCount[name]}")
+            assert commonCallCount == self._callCount[name]
+            avgDeltaCallTimesUs[name] = (self._callTimesUs[name]-self._callTimesUs['loopStartTime'])/self._callCount[name]
+            print(f'avgDeltaCallTimesUs[{name}]={avgDeltaCallTimesUs[name]}')
 
-    @printEntryAndExit
     def robotInit(self):
-        pass
+        self.count = 0
+        self._callTimesUs = {}
+        self._callCount = {}
+        self._callTimesUs['loopStartTime'] = 0
+        self._callTimesUs['disabledPeriodic'] = 0
+        self._callTimesUs['robotPeriodic'] = 0
+        self._callCount['loopStartTime'] = 0
+        self._callCount['disabledPeriodic'] = 0
+        self._callCount['robotPeriodic'] = 0
+        for i in range(NUM_TEST_PERIODIC):
+            name = f"{i}"
+            self.addPeriodic(lambda current_name=name: self.testPeriodic(f"{current_name}"),
+                             0.020, 0.000_001+0.000_001*i)
+            self._callTimesUs[name] = 0
+            self._callCount[name] = 0
+        gc.freeze()
+        gc.disable()
+
+
 
     @printEntryAndExit
+    def robotPeriodicBody(self, name):
+        self.count += 1
+        self.addCallLog('loopStartTime',self.getLoopStartTime())
+        if self.count > NUM_PERIODS:
+            self.endCompetition()
+
     def robotPeriodic(self):
-        pass
+        self.robotPeriodicBody('robotPeriodic')
 
-    @printEntryAndExit
     def autonomousInit(self):
         pass
 
-    @printEntryAndExit
     def autonomousPeriodic(self):
         pass
 
-    @printEntryAndExit
     def autonomousExit(self):
         pass
 
-    @printEntryAndExit
     def disabledInit(self):
         pass
 
     @printEntryAndExit
-    def disabledPeriodic(self):
+    def disabledPeriodicBody(self, name):
         pass
 
-    @printEntryAndExit
+    def disabledPeriodic(self):
+        self.disabledPeriodicBody('disabledPeriodic')
+
     def disabledExit(self):
         pass
 
-    @printEntryAndExit
     def _simulationInit(self):
         pass
 
-    @printEntryAndExit
     def _simulationPeriodic(self):
         pass
 
